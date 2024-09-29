b8c3ab9ade78334d4850bfd05eedcebdb1caaf9b
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-09-29 10:46:14 +0600

    color copy feature added

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ function getElementById(id){
 const root = getElementById("root");
 const btn = getElementById("change-color");
 const hexCode = getElementById("hexCode");
+const copyBtn = document.getElementById("copy");
 
 btn.addEventListener("click", function(){
     const bgColor = generateHexColor();
     root.style.backgroundColor = bgColor;
 
     hexCode.value = bgColor;
+    if(copyBtn.innerText === "Copied"){
+        copyBtn.innerText = "Copy";
+    }
 });
 
 
@@ -20,4 +24,11 @@ function generateHexColor(){
     const blue = Math.floor(Math.random() * 255);
     
     return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
+
+copyBtn.addEventListener("click", copyText);
+
+function copyText(){
+    navigator.clipboard.writeText(hexCode.value);
+    copyBtn.innerText = "Copied";
 }
