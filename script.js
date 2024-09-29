edfc5dc988f08e9017e9cacbce038c21bf85c409
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-09-29 11:48:23 +0600

    some modification done

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-function getElementById(id){
-    return document.getElementById(id);
-}
-
+let div = null;
 const root = getElementById("root");
 const btn = getElementById("change-color");
 const hexCode = getElementById("hexCode");
 const copyBtn = document.getElementById("copy");
+
+
+function getElementById(id){
+    return document.getElementById(id);
+}
 
 btn.addEventListener("click", function(){
     const bgColor = generateHexColor();
     root.style.backgroundColor = bgColor;
 
     hexCode.value = bgColor;
+    copyBtn.addEventListener("click", function(){div.style.backgroundColor = bgColor})
+    
     if(copyBtn.innerText === "Copied"){
         copyBtn.innerText = "Copy";
     }
 });
-
 
 function generateHexColor(){
     const red = Math.floor(Math.random() * 255);
@@ -31,4 +34,28 @@ copyBtn.addEventListener("click", copyText);
 function copyText(){
     navigator.clipboard.writeText(hexCode.value);
     copyBtn.innerText = "Copied";
+
+    if(div !== null){
+        div.remove();
+        div = null;
+    }
+    generateToastMessage(`${hexCode.value} copied`);
+}
+
+// toast message
+function generateToastMessage(msg){
+    div = document.createElement("div");
+    div.innerText = msg;
+    div.className = "fixed top-8 right-8 cursor-pointer bg-green-400 border border-white rounded-md px-4 py-3 shadow-md animation-in";
+
+    div.addEventListener("click", function(){
+        div.classList.remove("animation-in");
+        div.classList.add("animation-out");
+
+        div.addEventListener("animationend", function(){
+            div.remove();
+        })
+    })
+
+    document.body.appendChild(div);
 }
